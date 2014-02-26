a2e0190a56acccc09f78f634d2fb7366d5f04e65
Jkohli <jkohli@g2ft.com>
2014-02-26 16:41:11 -0500

    Initial page parsing w/ request and cheerio

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,26 @@
  */
 
 exports.index = function(req, res){
-  res.render('main', { title: 'MetroAlert' });
+
+var cheerio = require('cheerio');
+var request = require('request');fs = require('fs');
+
+	
+	lines = {
+		'123': 123,
+		'456': 456
+	}
+	
+	request('http://alert.mta.info/', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			
+			var $ = cheerio.load(body)
+			var status = $('div#L').text();
+			status = status.trim();
+			console.log(status);
+			res.send(body);
+		}		
+	})
+	
+	//res.render('main', { title: 'MetroAlert', status: 'On Time' });
 };
